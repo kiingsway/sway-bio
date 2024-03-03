adf56ecbf26cf665a5ec88901b720e8792770dbd
King Sway (Marcos Oranmiyan) <knucles0906@live.com>
2024-03-03 17:52:45 -0500

    Updating HTML Title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import type { AppProps } from 'next/app';
 import './_app.css';
+import type { AppProps } from 'next/app';
+import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <Head><title>Sway Bio</title></Head>
       <Analytics />
       <Component {...pageProps} />
     </>
